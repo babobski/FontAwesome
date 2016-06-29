3a6908e772d4ded1f698574df5a1fa95fa9dda4c
babobski <info@babobski.nl>
2016-06-29 23:07:19 +0200

    updated version numbers for release

--- content/overlay.js
+++ content/overlay.js
@@ -3,7 +3,7 @@
  */
 if (typeof(extensions) === 'undefined') extensions = {};
 if (typeof(extensions.FontAwesome) === 'undefined') extensions.FontAwesome = {
-	version: '1.4'
+	version: '2.1'
 };
 
 (function() {
